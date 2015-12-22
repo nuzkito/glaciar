7bd6dd4dbf578ce33827c0907e70cd66e2a52e01
abalozz <alivanprox@gmail.com>
2015-12-22 20:51:49 +0100

    Seeder para crear un usuario de prueba

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class)->create([
+            'email' => 'me@example.com',
+            'password' => bcrypt('secret'),
+        ]);
+    }
+}
