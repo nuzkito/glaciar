da1698e913ea36aaaeb757fdf1982b10a8287500
abalozz <alivanprox@gmail.com>
2016-04-23 20:36:30 +0200

    Seed para tener un usuario de cada rol. Todas las contraseñas del seed serán "secret".

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -15,7 +15,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
-        'password' => bcrypt(str_random(10)),
+        'password' => bcrypt('secret'),
         'role' => $faker->randomElement(['teacher', 'student']),
         'remember_token' => str_random(10),
     ];
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,15 +11,21 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class)->create([
+        factory(App\User::class, 'admin')->create([
             'name' => 'Admin',
-            'email' => 'admin@example.com',
-            'password' => bcrypt('secret'),
-            'role' => 'admin',
+            'email' => 'admin@example',
         ]);
 
-        factory(App\User::class, 49)->create([
-            'password' => bcrypt('secret'),
+        factory(App\User::class, 'teacher')->create([
+            'name' => 'Teacher',
+            'email' => 'teacher@example',
         ]);
+
+        factory(App\User::class, 'student')->create([
+            'name' => 'Student',
+            'email' => 'student@example',
+        ]);
+
+        factory(App\User::class, 50)->create();
     }
 }
